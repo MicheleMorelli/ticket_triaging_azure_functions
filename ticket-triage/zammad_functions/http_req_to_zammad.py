d82edea2a36ff0ad146399149795d26099d0cae2
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-06 18:34:23 +0000

    added put req for zammad

--- ticket-triage/zammad_functions/http_req_to_zammad.py
+++ ticket-triage/zammad_functions/http_req_to_zammad.py
@@ -19,12 +19,25 @@ def get_from_zammad(endpoint: str) -> str:
     req = requests.get(uri, auth=(usr, key))
     return req.json()
 
-
+'''
+used to create a ticket on Zammad
+'''
 def post_to_zammad(endpoint:str, payload:str) -> requests.models.Response:
     content_type = 'application/json'
     (usr,key,uri) = get_zammad_usr_key_uri(endpoint)
     params = {'Content-Type': content_type}
     req = requests.post(uri, headers=params, auth=(usr, key), data=payload )
+    return req
+
+
+'''
+used to update a ticket on Zammad
+'''
+def put_to_zammad(endpoint:str, payload:str) -> requests.models.Response:
+    content_type = 'application/json'
+    (usr,key,uri) = get_zammad_usr_key_uri(endpoint)
+    params = {'Content-Type': content_type}
+    req = requests.put(uri, headers=params, auth=(usr, key), data=payload )
     return req
 
 
