a4d6b02f3be6922a94fccdd1c3b82da8c8c20797
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-06 20:56:39 +0000

    more work on generic importer/ dependency injection

--- ticket-triage/helper/importer.py
+++ ticket-triage/helper/importer.py
@@ -4,8 +4,7 @@ dependency injection to keep the app flexible and ticketing-system independent.
 '''
 import importlib
 import configparser
-
-
+import sys
 '''
 Allows to import a module dynamically by indicating its name as a string
 '''
@@ -14,7 +13,19 @@ def import_from_string(module_name:str):
 
 
 def import_from_config(section:str, key:str):
+    __IMPORTER_PATH = sys.path[0]
+    __CONF_FILE = f"{__IMPORTER_PATH}/../conf/conf.ini"
     conf = configparser.ConfigParser()
-    conf.read('')
+    conf.read(__CONF_FILE)
+    return conf[section][key]
 
 
+def import_ticketing_system():
+    print (import_from_config('ticketing_system','name'))
+
+def main()->None:
+    import_ticketing_system()
+
+
+if __name__ == '__main__':
+    main()
