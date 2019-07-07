4b316be7282be00e45a8cc13dc29bd86448f23dd
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-07 00:01:36 +0000

    almost fixed the di module, now trying to make it work in azure

--- ticket-triage/helper/importer.py
+++ ticket-triage/helper/importer.py
@@ -3,9 +3,9 @@ A helper package that allows to import modules dynamically, and to perform
 dependency injection to keep the app flexible and ticketing-system independent.
 '''
 import importlib
+from importlib import util
 import configparser
 import sys
-import os
 
 __IMPORTER_PATH = sys.path[0]
 __CONF_FILE = f"{__IMPORTER_PATH}/../conf/conf.ini"
@@ -15,20 +15,23 @@ __PATH_TO_TICKETING_SYS = f"{__IMPORTER_PATH}/../ticketing_systems"
 Allows to import a module dynamically by indicating its name as a string
 '''
 def import_from_string(module_name:str):
-    module_name = f"{__PATH_TO_TICKETING_SYS}/{module_name}" 
     print(f"IMPORTING FROM {module_name}")
-    return importlib.import_module(module_name, package='connector')
+    sys.path.insert(0,__PATH_TO_TICKETING_SYS)
+    return importlib.import_module(module_name,package="connector")
+
+
 
 
 def import_from_config(section:str, key:str):
     conf = configparser.ConfigParser()
-    print(f"CONF FILE FOUND AT: {__CONF_FILE}")
     conf.read(__CONF_FILE)
+    print(f"CONF FILE FOUND AT: {__CONF_FILE}")
+    print(conf.sections())
     return import_from_string( conf[section][key])
 
 
 def import_ticketing_system():
-    print (import_from_config('ticketing_system','name'))
+    return import_from_config('ticketing_system','name')
 
 
 def main():
--- ticket-triage/ticket_classifier/__init__.py
+++ ticket-triage/ticket_classifier/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 import requests
 from helper import importer as di # Dependency injection via configuration
 
-#__TS = di.import_ticketing_system()
+__TS = di.import_ticketing_system()
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('It looks like that I received some tickets!')
