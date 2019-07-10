c60616b236fd79906946ebdd55ce2f9a83db317d
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-10 17:41:55 +0000

    added some doc

--- ticket-triage/ticketing_systems/zammad/poll_new_tickets.py
+++ ticket-triage/ticketing_systems/zammad/poll_new_tickets.py
@@ -4,11 +4,15 @@ import os
 import json
 import requests
 
-
+'''
+Returns all the tickets in the ticketing system that are marked as 'new'
+'''
 def get_new_tickets_from_ticketing_system_as_json() -> str:
     return json.dumps(zamreq.get_from_ticketing_system("tickets/search?query=state%3Anew").json())
     
-
+'''
+Makes a POST request to the azure function
+'''
 def post_to_azure(payload:str) -> requests.models.Response:
     content_type = 'application/json'
     uri = get_azure_uri()
@@ -17,12 +21,16 @@ def post_to_azure(payload:str) -> requests.models.Response:
     req = requests.post(uri, headers=params, data=payload )
     return req
 
-
-def get_azure_uri():
+'''
+Returns the Azure URI
+'''
+def get_azure_uri()->str:
     return os.getenv('AZURE_CLASSIFIER_URI')
 
-
-def main():
+'''
+Sends all the new tickets to Azure for processing.
+'''
+def main()->None:
     payload = get_new_tickets_from_ticketing_system_as_json()
     resp = post_to_azure(payload)
     print(resp)
