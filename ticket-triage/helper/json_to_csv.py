b3c60530698ebe00dd8b487ecd9a0bdcdf32e235
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-13 12:40:26 +0000

    working on the josn-csv converter

--- ticket-triage/helper/json_to_csv.py
+++ ticket-triage/helper/json_to_csv.py
@@ -13,11 +13,21 @@ def json_to_csv(filename:str):
     jsonfile = {}
     with open(filename,'r') as fh:
         jsonfile = json.load(fh)
-    print(jsonfile)
+    csv_file = csv.writer(open(f"{filename.replace('.json','.csv')}", "w"))
+    # headers
+    #id,summary,board,company,type,subtype,priority,customFields
+    #headers = ['id','summary','board', 'company','type','subtype','priority']
+    #print(jsonfile['descriptions'][0].keys())
+    csv_file.writerow(jsonfile['descriptions'][0].keys())
+    for ticket in jsonfile['descriptions']:
+        csv_file.writerow(ticket.values())
+
+    
 
 
 def main():
-    json_
+    json_file=f"{PATH_TO_JSON_FILES}/desc_5_7000_7193.json"
+    json_to_csv(json_file)
 
 if __name__ == '__main__':
     main()
