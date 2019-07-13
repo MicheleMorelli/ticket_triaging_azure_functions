27db4435ca65628abd781e73ee925663eaf82df1
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-13 12:04:09 +0000

    progress on json csv conversion

--- ticket-triage/helper/json_to_csv.py
+++ ticket-triage/helper/json_to_csv.py
@@ -7,23 +7,18 @@ import json
 import csv
 from typing import List, Dict, Any 
 
-def import_csv_as_dict(filename:str) -> Dict[str,str]:
-    csv_dict: Dict[str,List[str]] = {"elements":[]}
-    with open(filename, 'r') as fh:
-        labels:List[str] = get_csv_headers(filename)
-        for row in csv.DictReader(fh):
-            d:Dict[str,str] = {}
-            d = {label:row[label] for label in labels}
-            csv_dict["elements"].append(d)
-    return csv_dict
+PATH_TO_JSON_FILES='../datasets'
+
+def json_to_csv(filename:str):
+    jsonfile = {}
+    with open(filename,'r') as fh:
+        jsonfile = json.load(fh)
+    print(jsonfile)
 
 
-def convert_csv_into_json(filename:str)->str:
-    return json.dumps(import_csv_as_dict(filename))
+def main():
+    json_
 
+if __name__ == '__main__':
+    main()
 
-def get_csv_headers(filename:str)->List[str]:
-    headers = []
-    with open(filename, 'r') as fh:
-        headers = csv.reader(fh).__next__()
-    return headers
