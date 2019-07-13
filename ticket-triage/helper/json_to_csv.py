c49abd0d528387b2b373ad4fbb60d80832b16f28
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-13 13:04:25 +0000

    working on collating the json descriptions

--- ticket-triage/helper/json_to_csv.py
+++ ticket-triage/helper/json_to_csv.py
@@ -14,20 +14,69 @@ def json_to_csv(filename:str):
     with open(filename,'r') as fh:
         jsonfile = json.load(fh)
     csv_file = csv.writer(open(f"{filename.replace('.json','.csv')}", "w"))
-    # headers
-    #id,summary,board,company,type,subtype,priority,customFields
-    #headers = ['id','summary','board', 'company','type','subtype','priority']
-    #print(jsonfile['descriptions'][0].keys())
-    csv_file.writerow(jsonfile['descriptions'][0].keys())
-    for ticket in jsonfile['descriptions']:
+    csv_file.writerow(jsonfile['tickets'][0].keys())
+    for ticket in jsonfile['tickets']:
         csv_file.writerow(ticket.values())
+'''
+id
+summary
+board
+company_id
+company_name
+
+type_id
+type_name
+subtype_id
+subtype_name
+
+priority_id
+priority_name
+
+#####customFields#######
+
+product
+product_area
+urgency
+
+##### notes #####
+description
+'''
+def collate_json_files():
+    pass
 
     
 
+'''
+Inifies all the small desc json files into a unique one
+'''
+def unite_desc_json_files():
+    desc_json_files=[
+            "desc_1_0_2999.json",
+            "desc_4_6500_6999.json",
+            "desc_2_3000_5999.json",
+            "desc_3_6000_6499.json",
+            "desc_5_7000_7193.json"]
+    
+    for f in desc_json_files:
+        with open(f"{PATH_TO_JSON_FILES}/{f}", 'r') as jf:
+            d = json.load(jf)
+            print(len(d['descriptions']))
+    
+
+    
+    #output_desc_json_filename='all_descriptions.json'
+    #output_string = ""
+    #with open(output_desc_json_filename, 'w') as fh:
+        
+        
+
+
+
 
 def main():
-    json_file=f"{PATH_TO_JSON_FILES}/desc_5_7000_7193.json"
-    json_to_csv(json_file)
+    #json_file=f"{PATH_TO_JSON_FILES}/desc_5_7000_7193.json"
+    #json_to_csv(json_file)
+    unite_desc_json_files()
 
 if __name__ == '__main__':
     main()
