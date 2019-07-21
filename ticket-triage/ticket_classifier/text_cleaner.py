211fa3ea042549c1cba3606b06b9265daaada733
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-21 11:28:12 +0000

    improved numbers regexes, and added twitter and relevant URL function

--- ticket-triage/ticket_classifier/text_cleaner.py
+++ ticket-triage/ticket_classifier/text_cleaner.py
@@ -7,13 +7,16 @@ from toolz import pipe
 
 '''
 Takes a ticket description as an argument, and passes it though a functional 
-pipeline, that sanitises, tokenise and lemmatise it.
+pipeline, that sanitises, tokenise and lemmatise it, and returns as a list of 
+strings.
 '''
 def functional_cleaner(ticket_description:str)->List[str]:
     return pipe( 
             re.split(r'\s+', ticket_description.lower()), # the input list (*TO LOWER CASE!*)
             remove_empty_strings,
             remove_numbers,
+            transform_relevant_URLs_into_tags,
+            remove_twitter_contacts,
             remove_email_addresses,
             remove_URLs,
             remove_stopwords,
@@ -42,10 +45,10 @@ def remove_empty_strings(s_list:List[str])->List[str]:
 
 
 '''
-Removes strings composed only by numbers from a list of strings
+Removes strings composed by numbers from a list of strings
 '''
 def remove_numbers(s_list:List[str])->List[str]:
-    num_regex = r'\d+'
+    num_regex = r'^[0-9]+$'
     is_number = lambda x: re.search(num_regex,x)
     return remove_if(is_number, s_list)
 
@@ -58,12 +61,73 @@ def remove_email_addresses(s_list:List[str])->List[str]:
     is_email_address = lambda x: re.search(email_regex,x)
     return remove_if(is_email_address, s_list)
 
+
+'''
+Removes email addresses from a list of strings
+'''
+def remove_twitter_contacts(s_list:List[str])->List[str]:
+    twitter_regex = r'^@[a-z0-9_-]+$'
+    is_twitter_contact = lambda x: re.search(twitter_regex,x)
+    return remove_if(is_twitter_contact, s_list)
+
+
+
+'''
+Takes a list of strings, and checks whether any string contains a URL relevant
+to one of the services (Moodle or Open Access repositories), and if so, 
+it transforms it into a recognisable tag.
+'''
+def transform_relevant_URLs_into_tags(s_list:List[str])->List[str]:
+    transform_relevant_URL = lambda x: turn_into_tag(x) if (is_relevant_URL(x)) else x
+    return list(map(transform_relevant_URL, s_list))
+
+
+def turn_into_tag(s:str)->str:
+    return "BLOOMPOTENTIALURL" if is_bloom_URL(s) else "RESTECHPOTENTIALURL"
+
+
+def is_relevant_URL(s:str)->bool:
+    return is_a_URL(s) and (is_bloom_URL(s) or is_restech_URL(s))
+    
+
+def is_bloom_URL(s:str)->bool:
+    return any(filter(lambda x: re.search(re.escape(x), s), bloom_words()))
+    
+
+def is_restech_URL(s:str)->bool:
+    return any(filter(lambda x: re.search(re.escape(x), s), restech_words()))
+
+'''
+Returns a list of Moodle-related words that are often used in URLs
+TODO: Expand list!
+'''
+def bloom_words()->List[str]:
+    return['bloom','vle','moodle','ble']
+
+
+'''
+Returns a list of Open Access repository-related words that are often used in URLs
+TODO: Expand list!
+'''
+def restech_words()->List[str]:
+    return[
+            'eprint',
+            'research',
+            'researchdata',
+            'repository', 
+            'open', 
+            'access',
+            'publications',
+            'data'
+            ]
+
+
 '''
 Checks if a string is a URL
 TODO: improve regex
 '''
 def is_a_URL(s:str)->bool:
-    URL_regex = r'(https?:\/\/?(www\.))?[a-z0-9./_-]+\.(com|ac\.uk|org|co\.uk|gov)'
+    URL_regex = r'(https?:\/\/)?(www\.)?[a-z0-9./_-]+\.(com|ac\.uk|org|co\.uk|gov|net)(\/.*)?'
     return re.search(URL_regex,s)
 
 
@@ -118,10 +182,14 @@ def wordnet_pos_mapper(s:str):
 
 
 s='''
+
 '''
 
-#cleaned = clean_ticket_description(s)
-#print(cleaned)
-#print(len(cleaned))
-#print(remove_email_addresses(re.split(r'\s+', s)))
 print(functional_cleaner(s))
+
+#url1 = 'https://moodle.royalholloway.ac.uk/course/view.php?id=6477' 
+#url2 = '[https://research.royalholloway.ac.uk/course/view.php?id=6477]'
+#print(is_relevant_URL(url1))
+#print(is_relevant_URL(url2))
+#print(transform_relevant_URLs_into_tags([url1,url2,"asdas","sdfsdf"]))
+
