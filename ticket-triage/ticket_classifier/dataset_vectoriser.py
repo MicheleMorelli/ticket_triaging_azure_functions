74bf746324545e03a0f964c7c71de1656ee0ea46
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-27 13:11:20 +0000

    more work on the vectoriser

--- ticket-triage/ticket_classifier/dataset_vectoriser.py
+++ ticket-triage/ticket_classifier/dataset_vectoriser.py
@@ -7,15 +7,14 @@ from typing import Dict, List
 
 import text_cleaner as tc
 
-def get_vectoriser_and_vectorised_training_dataset(dataset:pd.DataFrame,fieldname:str):
-    # the following list comprehension is to deal with the Pandas' NaN values
-    all_rows = ["" if type(x) == float else x for x in dataset[fieldname]]
-    vect = TfidfVectorizer(analyzer=tc.functional_cleaner)
-    vectorised_dataset = vect.fit_transform(all_rows)
-    return vect, vectorised_dataset 
     
-def get_fit_vectoriser(dataset:pd.DataFrame)->List[str]:
-    all_rows = ["" if type(x) == float else x for x in dataset[fieldname]]
+'''
+Gets a training as a list of strings, and returns a fitted TD-IDF vectoriser
+'''
+def get_fitted_vectoriser(dataset:List[str])->TfidfVectorizer:
+    vect = TfidfVectorizer(analyzer=tc.functional_cleaner)
+    vect.fit(dataset)
+    return vect
 
 '''
 Takes a Pandas Series, and returns a list of strings. This was to solve the 
@@ -32,6 +31,7 @@ def import_dataset_from_csv_as_panda_dataframe(filename:str)->pd.DataFrame:
 
 if __name__ == '__main__':
     pd.set_option('display.max_colwidth',50)
-    dataset = import_dataset_from_csv_as_panda_dataframe('14-07CLEANED_dataset.csv')
-    vect, vectorised_dataset = get_vectoriser_and_vectorised_training_dataset(dataset, 'summary')
-    print(vectorised_dataset)
+    full_dataset = import_dataset_from_csv_as_panda_dataframe('14-07CLEANED_dataset.csv')
+    vect = get_fitted_vectoriser(convert_pd_series_with_NaN_to_string_list(full_dataset['summary']))
+    vectorised_data = vect.transform(full_dataset['summary'])
+
