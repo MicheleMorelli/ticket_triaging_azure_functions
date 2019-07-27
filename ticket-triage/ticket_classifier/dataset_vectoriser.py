d3816c5c8ad1e8670bb7f5338b2e8d99fedde55b
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-27 14:26:24 +0000

    pickle-aided workflow ok

--- ticket-triage/ticket_classifier/dataset_vectoriser.py
+++ ticket-triage/ticket_classifier/dataset_vectoriser.py
@@ -30,17 +30,29 @@ def import_dataset_from_csv_as_panda_dataframe(filename:str)->pd.DataFrame:
     FILENAME=filename
     return pd.read_csv(f"{_PATH}/{FILENAME}")
 
-def dump_fitted_vectoriser_to_pickle(pickle_filename:str,series:pd.Series)->None:
+def dump_fitted_vectoriser_to_pickle(field:str,series:pd.Series)->None:
     _PATH="../datasets/pickle"
     data = convert_pd_series_with_NaN_to_string_list(series)
     vect = get_new_fitted_vectoriser(data)
-    pickle.dump(vect, open(f"{_PATH}/{pickle_filename}", 'wb'))
-    print(f"Dumped fitted vectoriser to {_PATH}/{pickle_filename}!")
+    pickle.dump(vect, open(f"{_PATH}/{field}_tfidf_vectoriser.pickle", 'wb'))
+    print(f"Dumped fitted vectoriser to {_PATH}/{field}_tfidf_vectoriser.pickle!")
 
+
+def dump_vectorised_data_to_pickle(vect:TfidfVectorizer,field:str,series:pd.Series)->None:
+    _PATH="../datasets/pickle"
+    data = convert_pd_series_with_NaN_to_string_list(series)
+    pickle.dump(vect.transform(data), open(f"{_PATH}/{field}_vectorised_data.pickle", 'wb'))
+    print(f"Dumped vectorised_data to {_PATH}/{field}_vectorised_data.pickle!")
 
 def load_vectoriser_from_pickle(field:str)->TfidfVectorizer:
     _PATH="../datasets/pickle"
     return pickle.load( open( f"{_PATH}/{field}_tfidf_vectoriser.pickle", "rb" ) )
+
+
+def load_vectorised_data_from_pickle(field:str):
+    _PATH="../datasets/pickle"
+    return pickle.load( open( f"{_PATH}/{field}_vectorised_data.pickle", "rb" ) )
+
 
 '''
 Dumps a pickled fitted vectoriser for each of the fields in the list passed as an argument
@@ -49,15 +61,27 @@ It may take quite long!
 '''
 def dump_all_vectorisers_to_pickle(full_dataset:pd.DataFrame, fields_list:List[str])->None:
     for field in fields_list:
-        dump_fitted_vectoriser_to_pickle(f"{field}_tfidf_vectoriser.pickle", full_dataset[field])
+        dump_fitted_vectoriser_to_pickle(f"{field}", full_dataset[field])
+
+
+'''
+Dumps a a vectorised dataset to pickle for each of the fields in the list passed as an argument
+Useful when changing and testing the engineered features.
+It may definitely take quite long!
+'''
+def dump_all_vectorised_data_to_pickle(vect:TfidfVectorizer,full_dataset:pd.DataFrame, fields_list:List[str])->None:
+    for field in fields_list:
+        dump_vectorised_data_to_pickle(vect, f"{field}", full_dataset[field])
 
 
 if __name__ == '__main__':
     pd.set_option('display.max_colwidth',50)
     full_dataset = import_dataset_from_csv_as_panda_dataframe('14-07CLEANED_dataset.csv')
-    #dump_all_vectorisers_to_pickle(full_dataset, ['summary', 'description'])
+    field_list = ['summary','description']
+    #dump_all_vectorisers_to_pickle(full_dataset, field_list)
     vect = load_vectoriser_from_pickle("description")
-    vectorised_data = vect.transform(full_dataset['summary'][0:50])
-    print(vectorised_data.shape)
+    dump_all_vectorised_data_to_pickle(vect, full_dataset, field_list)
+    #vectorised_data = load_vectorised_data_from_pickle('summary')
+    #print(vectorised_data.shape)
 
 
