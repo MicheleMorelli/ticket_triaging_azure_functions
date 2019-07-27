a726336423b8f13a4a06f9917f55780d00f026b9
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-27 16:31:23 +0000

    more on classifier

--- ticket-triage/ticket_classifier/ticket_classifier.py
+++ ticket-triage/ticket_classifier/ticket_classifier.py
@@ -9,15 +9,29 @@ from dataset_vectoriser import convert_pd_series_with_NaN_to_string_list as get_
 
 PICKLE_PATH="../datasets/pickle"
 
+
 if __name__ == '__main__':
     full_dataset = get_dataset('14-07CLEANED_dataset.csv')
-    fieldname = 'board_name'
-    target_class = get_as_str_list(full_dataset[fieldname])
-    features_pickle_filename = f"{PICKLE_PATH}/description_vectorised_data_20190727_1507.pickle"
-    vectorised_dataset = pickle.load(open(features_pickle_filename, "rb"))
-    training_data, test_data, training_target, test_target = train_test_split(vectorised_dataset, target_class, test_size = 0.25, random_state = 23)
-    classifier = MultinomialNB().fit(training_data, training_target)
-    predicter = classifier.predict(test_data)
-    report = metrics.classification_report(test_target, predicter)
-    print(report)
+    fieldnames = ['board_name', "type_name", "subtype_name","product", "product_area"]
+    for fieldname in fieldnames:
+        target_class = get_as_str_list(full_dataset[fieldname])
+        features_pickle_filename = f"{PICKLE_PATH}/description_vectorised_data_20190727_1507.pickle"
+        vectorised_dataset = pickle.load(open(features_pickle_filename, "rb"))
+        training_data, test_data, training_target, test_target = train_test_split(vectorised_dataset, target_class, test_size = 0.25, random_state = 23)
+        classifier = MultinomialNB().fit(training_data, training_target)
+        # Vectoriser
+        vect_pickle_filename = f"{PICKLE_PATH}/description_tfidf_vectoriser_20190727_1507.pickle"
+        vect = pickle.load(open(vect_pickle_filename, "rb"))
+        
+        # ===============================================================
+        #training accuracy tests
+        prediction = classifier.predict(test_data)
+        #report = metrics.classification_report(test_target, prediction)
+        accuracy = metrics.accuracy_score(test_target, prediction)
+        print(f"TARGET LABEL {fieldname.upper()}:")
+        print(f"Accuracy: {round(accuracy*100,2)}%")
+        #=================================================================
 
+        #new_ticket = ["Ref is not working"]
+        #prediction = classifier.predict(vect.transform(new_ticket))
+        #print(f"TARGET LABEL {fieldname.upper()}: => {prediction[0]}")
