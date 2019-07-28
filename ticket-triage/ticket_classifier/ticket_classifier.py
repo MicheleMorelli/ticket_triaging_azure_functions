eb359943e8bb80d317f6b58a1bdc8c240d57403d
MicheleMorelli <morelli.michele@hotmail.it>
2019-07-28 20:44:05 +0000

    predicter ok

--- ticket-triage/ticket_classifier/ticket_classifier.py
+++ ticket-triage/ticket_classifier/ticket_classifier.py
@@ -35,32 +35,53 @@ def split_train_test_model(full_dataset, fieldnames:List[str]):
     vect = get_vectoriser()
     vectorised_dataset = get_vectorised_dataset()
     # create directory for pickled classifiers
-    CLASSIFIER_DIRNAME = f"{PICKLE_CLASSIFIER_PATH}/{time.strftime('%Y%m%d_%H%M')}"
+    CLASSIFIER_DIRNAME = f"{PICKLE_CLASSIFIER_PATH}/{time.strftime('%Y%m%d%H%M')}"
     if not os.path.exists(CLASSIFIER_DIRNAME):
             os.makedirs(CLASSIFIER_DIRNAME)
     for fieldname in fieldnames:
         target_class = get_as_str_list(full_dataset[fieldname])
         training_data, test_data, training_target, test_target = train_test_split(vectorised_dataset, target_class, test_size = 0.25)
         classifier = MultinomialNB().fit(training_data, training_target)
-
         #dump this classifier in the pickle dir
         pickle.dump(classifier, open(f"{CLASSIFIER_DIRNAME}/{fieldname}_classifier.pickle",'wb'))
-        
         # ===============================================================
         #training accuracy tests
-        #prediction = classifier.predict(test_data)
+        prediction = classifier.predict(test_data)
         #report = metrics.classification_report(test_target, prediction)
-        #accuracy = metrics.accuracy_score(test_target, prediction)
-        #print(f"TARGET LABEL {fieldname.upper()}:")
-        #print(f"Accuracy: {round(accuracy*100,2)}%")
+        accuracy = metrics.accuracy_score(test_target, prediction)
+        print(f"TARGET LABEL {fieldname.upper()}:")
+        print(f"Accuracy: {round(accuracy*100,2)}%")
         #=================================================================
 
-        new_ticket = ["contract expiring"]
-        prediction = classifier.predict(vect.transform(new_ticket))
-        print(f"TARGET LABEL {fieldname.upper()}: => {prediction[0]}")
+
+
+def predict_ticket_labels(description:str, target_fields:List[str])->Dict[str,str]:
+    labels = {}
+    vect = get_vectoriser()
+    for field in target_fields:
+        #get the specific current classifier
+        classifier_dir = conf.get_config('pickle_files','current_classifier')
+        classifier_name = f"{PICKLE_CLASSIFIER_PATH}/{classifier_dir}/{field}_classifier.pickle"
+        classifier = pickle.load(open(classifier_name, "rb"))
+        if classifier:
+            prediction = classifier.predict(vect.transform([description]))
+            labels[field] = prediction[0]
+    return labels
+
 
 
 if __name__ == '__main__':
     full_dataset = get_dataset(conf.get_config('classifier_datasets','full_dataset'))
     fieldnames = ['board_name', "type_name", "subtype_name","product", "product_area"]
-    split_train_test_model(full_dataset,fieldnames)
+    test_ticket = '''
+    Hi, 
+    our Eprints repository does not work anymore.
+    Thank you,
+    Michele
+    '''
+    
+    # Uncomment the next line to train and test the model
+    #split_train_test_model(full_dataset,fieldnames)
+    
+    #uncomment the next line to test the new ticket prediction
+    print(predict_ticket_labels(test_ticket,fieldnames))
