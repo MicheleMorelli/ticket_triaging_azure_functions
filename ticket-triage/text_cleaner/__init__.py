167d73a5a8d3ea80a411d60d653113bb50f1f71c
MicheleMorelli <morelli.michele@hotmail.it>
2019-08-09 21:13:44 +0000

    reverted autopep8 and reformatted a bit

--- ticket-triage/text_cleaner/__init__.py
+++ ticket-triage/text_cleaner/__init__.py
@@ -1,12 +1,11 @@
 import re
 import string
-import typing  # for TypeAliases/pseudotypes such as Match
+import typing # for TypeAliases/pseudotypes such as Match
 from typing import *
 import nltk
 from toolz import pipe
 
 _LEMMATISER = nltk.WordNetLemmatizer()
-
 
 def functional_cleaner(ticket_description: str) -> List[str]:
     '''
@@ -14,29 +13,28 @@ def functional_cleaner(ticket_description: str) -> List[str]:
     pipeline, that sanitises, tokenise and lemmatise it, and returns as a list of 
     strings.
     '''
-    return pipe(
-        # the input list (*TO LOWER CASE!*)
-        re.split(r'\s+', ticket_description.lower()),
-        transform_relevant_URLs_into_tags,
-        remove_twitter_contacts,
-        remove_email_addresses,
-        remove_URLs,
-        remove_non_printable_hex,
-        remove_stopwords,
-        remove_punctuation,
-        remove_non_ASCII_chars,
-        remove_ticket_references,
-        # NUMBERS ===============================
-        # remove_numbers,
-        # remove_strings_starting_with_numbers,
-        remove_strings_containing_any_numbers,  # may be too much!
-        remove_very_long_words,
-        remove_very_short_words,
-        remove_empty_strings,  # don't pass empty strings to the lemmatizer!
-        remove_names,
-        remove_residual_URLs,
-        lemmatize_all
-    )
+    return pipe( 
+            re.split(r'\s+', ticket_description.lower()), # the input list (*TO LOWER CASE!*)
+            transform_relevant_URLs_into_tags,
+            remove_twitter_contacts,
+            remove_email_addresses,
+            remove_URLs,
+            remove_non_printable_hex,
+            remove_stopwords,
+            remove_punctuation,
+            remove_non_ASCII_chars,
+            remove_ticket_references,
+            #NUMBERS ===============================
+            #remove_numbers,
+            #remove_strings_starting_with_numbers,
+            remove_strings_containing_any_numbers, # may be too much!
+            remove_very_long_words,
+            remove_very_short_words,
+            remove_empty_strings, # don't pass empty strings to the lemmatizer!
+            remove_names,
+            remove_residual_URLs,
+            lemmatize_all
+            )
 
 
 def remove_if(predicate, string_list):
@@ -52,8 +50,8 @@ def remove_empty_strings(s_list: List[str]) -> List[str]:
     '''
     Removes empty strings from a list of strings.
     '''
-    def is_empty(x): return not x
-    return remove_if(is_empty, s_list)
+    is_empty = lambda x: not x
+    return remove_if(is_empty,s_list)
 
 
 def remove_strings_containing_any_numbers(s_list: List[str]) -> List[str]:
@@ -61,7 +59,7 @@ def remove_strings_containing_any_numbers(s_list: List[str]) -> List[str]:
     Remove all the strings that contain a number. Might be too extreme!
     '''
     num_regex = r'[0-9]+'
-    def is_number(x): return re.search(num_regex, x)
+    is_number = lambda x: re.search(num_regex,x)
     return remove_if(is_number, s_list)
 
 
@@ -70,8 +68,10 @@ def remove_non_printable_hex(s_list: List[str]) -> List[str]:
     Removes escaped hexadecimals such as \x43
     '''
     hex_regex = r'\\x\d+'
-    def is_hex(x): return re.search(hex_regex, x)
+    is_hex = lambda x: re.search(hex_regex,x)
     return remove_if(is_hex, s_list)
+
+
 
 
 def remove_numbers(s_list: List[str]) -> List[str]:
@@ -79,7 +79,7 @@ def remove_numbers(s_list: List[str]) -> List[str]:
     Removes strings composed by *only* numbers from a list of strings
     '''
     num_regex = r'^[0-9]+$'
-    def is_number(x): return re.search(num_regex, x)
+    is_number = lambda x: re.search(num_regex,x)
     return remove_if(is_number, s_list)
 
 
@@ -88,9 +88,9 @@ def remove_strings_starting_with_numbers(s_list: List[str]) -> List[str]:
     Removes strings starting with numbers from a list of strings
     '''
     start_with_num_regex = r'^[0-9]+'
-    def starts_with_number(x): return re.search(start_with_num_regex, x)
+    starts_with_number = lambda x: re.search(start_with_num_regex,x)
     return remove_if(starts_with_number, s_list)
-
+    
 
 def remove_punctuation(s_list: List[str]) -> List[str]:
     '''
@@ -111,8 +111,8 @@ def remove_very_long_words(s_list: List[str]) -> List[str]:
     Removes very long words. Especially useful to remove hashes, residual URLs and 
     other anomalies
     '''
-    long_threshold = 25  # this is arbitrary, and has been adjusted by checking the results
-    def is_a_very_long_word(x): return len(x) >= long_threshold
+    long_threshold = 25 # this is arbitrary, and has been adjusted by checking the results
+    is_a_very_long_word = lambda x: len(x) >= long_threshold
     return remove_if(is_a_very_long_word, s_list)
 
 
@@ -120,8 +120,8 @@ def remove_very_short_words(s_list: List[str]) -> List[str]:
     '''
     Removes very short words. 
     '''
-    short_threshold = 2  # this is arbitrary, and has been adjusted by checking the results
-    def is_a_very_short_word(x): return len(x) <= short_threshold
+    short_threshold = 2 # this is arbitrary, and has been adjusted by checking the results
+    is_a_very_short_word = lambda x: len(x) <= short_threshold
     return remove_if(is_a_very_short_word, s_list)
 
 
@@ -129,9 +129,10 @@ def remove_ticket_references(s_list: List[str]) -> List[str]:
     '''
     Removes ticket identifiers
     '''
-    ticketid_regex = r'^inc\d+$'  # Service now format
-    def is_a_ticket_identifier(x): return re.search(ticketid_regex, x)
+    ticketid_regex = r'^inc\d+$' # Service now format
+    is_a_ticket_identifier = lambda x: re.search(ticketid_regex,x)
     return remove_if(is_a_ticket_identifier, s_list)
+
 
 
 def remove_email_addresses(s_list: List[str]) -> List[str]:
@@ -139,7 +140,7 @@ def remove_email_addresses(s_list: List[str]) -> List[str]:
     Removes email addresses from a list of strings
     '''
     email_regex = r'[a-z0-9_.-]+?@[a-z0-9_-]+\.[a-z0-9]'
-    def is_email_address(x): return re.search(email_regex, x)
+    is_email_address = lambda x: re.search(email_regex,x)
     return remove_if(is_email_address, s_list)
 
 
@@ -148,8 +149,9 @@ def remove_twitter_contacts(s_list: List[str]) -> List[str]:
     Removes email addresses from a list of strings
     '''
     twitter_regex = r'^@[a-z0-9_-]+$'
-    def is_twitter_contact(x): return re.search(twitter_regex, x)
+    is_twitter_contact = lambda x: re.search(twitter_regex,x)
     return remove_if(is_twitter_contact, s_list)
+
 
 
 def transform_relevant_URLs_into_tags(s_list: List[str]) -> List[str]:
@@ -158,8 +160,7 @@ def transform_relevant_URLs_into_tags(s_list: List[str]) -> List[str]:
     to one of the services (Moodle or Open Access repositories), and if so, 
     it transforms it into a recognisable tag.
     '''
-    def transform_relevant_URL(x): return turn_into_tag(
-        x) if (is_relevant_URL(x)) else x
+    transform_relevant_URL = lambda x: turn_into_tag(x) if (is_relevant_URL(x)) else x
     return list(map(transform_relevant_URL, s_list))
 
 
@@ -168,15 +169,17 @@ def turn_into_tag(s: str) -> str:
 
 
 def is_relevant_URL(s: str) -> Union[typing.Match[str], None, bool]:
-    return re.search(r'\.ac\.uk$', s) and ((is_bloom_URL(s) or is_restech_URL(s)))
-
+    return re.search(r'\.ac\.uk$', s) and (is_bloom_URL(s) or is_restech_URL(s))
+    
 
 def is_bloom_URL(s: str) -> Optional[typing.Match[str]]:
     return re.search(r'bloom|vle|moodle|ble', s)
-
+    
 
 def is_restech_URL(s: str) -> Optional[typing.Match[str]]:
-    return re.search(r'eprint|research|researchdata|repository|open|access|publications|data', s)
+    return re.search(r'eprint|research|researchdata|repository|open|access|publications|data'
+, s)
+
 
 
 def is_a_URL(s: str) -> Optional[typing.Match[str]]:
@@ -186,7 +189,8 @@ def is_a_URL(s: str) -> Optional[typing.Match[str]]:
     Daveo (username), https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url 
     '''
     URL_regex = r'[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)'
-    return re.search(URL_regex, s)
+    return re.search(URL_regex,s)
+
 
 
 def remove_URLs(s_list: List[str]) -> List[str]:
@@ -200,25 +204,23 @@ def remove_residual_URLs(s_list: List[str]) -> List[str]:
     '''
     Removes residual URLs (which for some reason were not removed in previous steps)
     '''
-    def is_residual_URL(x): return re.search(
-        r'^www', x) or re.search(r'acuk$', x)
+    is_residual_URL = lambda x: re.search(r'^www',x) or re.search(r'acuk$',x)
     return remove_if(is_residual_URL, s_list)
-
 
 def remove_stopwords(s_list: List[str]) -> List[str]:
     '''
     Removes English stopwords from a list of strings
     '''
-    def is_a_stopword(x): return x in nltk.corpus.stopwords.words('english')
+    is_a_stopword = lambda x: x in nltk.corpus.stopwords.words('english')
     return remove_if(is_a_stopword, s_list)
-
+    
 
 def remove_names(s_list: List[str]) -> List[str]:
     '''
     Remove names from a list of strings.
     '''
     names = [x.lower() for x in nltk.corpus.names.words()]
-    def is_a_name(x): return x in names
+    is_a_name = lambda x: x in names 
     return remove_if(is_a_name, s_list)
 
 
@@ -231,14 +233,14 @@ def lemmatize_all(s_list: List[str]) -> List[str]:
     return [lem.lemmatize(x, wordnet_pos_mapper(x)) for x in s_list]
 
 
+
 def wordnet_pos_mapper(s: str):
     '''
     Returns the nltk.wordnet POS_tag of the word passed as an argument.
     This is used by the WordNet lemmatizer in the pipeline.
     '''
-    pos_map = {"n": "NOUN", "v": "VERB", "r": "ADV", "j": "ADV"}
+    pos_map = {"n": "NOUN","v": "VERB","r": "ADV", "j":"ADV"}
     pos = nltk.pos_tag([s])[0][1][0].lower()
-    # Return 'n' in case it's anything that is not in pos_map
-    pos = pos_map.get(pos, "NOUN")
-    tag = getattr(nltk.wordnet.wordnet, pos)
+    pos = pos_map.get(pos,"NOUN") # Return 'n' in case it's anything that is not in pos_map
+    tag = getattr(nltk.wordnet.wordnet,pos)
     return tag
