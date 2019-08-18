3d664c610fc0c32091a72efa458cb3fcb9afd632
MicheleMorelli <morelli.michele@hotmail.it>
2019-08-18 15:17:06 +0000

    concurrency tests

--- ticket-triage/tests/test_concurrency.py
+++ ticket-triage/tests/test_concurrency.py
@@ -2,16 +2,42 @@
 Testing concurrency non-functional requirement
 """
 
-import threading
+from threading import Thread
+import os
 import sys
 import requests
 sys.path.append('../')
 
 
+def make_req() -> requests.models.Response:
+    payload = '[{"ticket_id":30,"summary": "summary","description": "description"}]'
+    return post_to_azure(payload).status_code
+
+def test_concurrent() -> None:
+    calls_num = 100
+    for i in range(0,calls_num):
+        t = Thread(target=make_req)
+        t.start()
 
 
-def azure_uri() -> str:
+def post_to_azure(payload: str) -> requests.models.Response:
+    """
+    Makes a POST request to the azure function
+    """
+    content_type = 'application/json'
+    uri = get_azure_uri()
+    print(payload)
+    params = {'Content-Type': content_type}
+    req = requests.post(uri, headers=params, data=payload )
+    return req
+
+
+def get_azure_uri() -> str:
+    """
+    Returns the Azure URI
+    """
     return os.getenv('AZURE_CLASSIFIER_URI')
 
 
-
+if __name__ == '__main__':
+    test_concurrent()
